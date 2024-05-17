516f1ffeeb00286b583a8ee702f48db25f6d12ba
irving2delgado <irving2delgado@gmail.com>
2024-05-17 12:39:35 -0600

    Final Styles

--- views/UserView.js
+++ views/UserView.js
@@ -9,8 +9,8 @@ class UserView {
       const userCard = $(`
         <div class="user-card">
           <p><strong>Nombre:</strong> ${user.name}</p>
-          <p><strong>Email:</strong> ${user.email}</p>
-          <p><strong>Teléfono:</strong> ${user.phone}</p>
+          <p class="email"><strong><i class="fas fa-envelope"></i> Email:</strong> ${user.email}</p>
+          <p class="phone"><strong><i class="fas fa-phone"></i> Teléfono:</strong> ${user.phone}</p>
         </div>
       `);
       this.userList.append(userCard);
